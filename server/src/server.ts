6cee6c4fc32abdfad3fd4608b39fec98ea07b67e
gabrielcasag <gaspericasag@gmail.com>
2023-05-19 02:11:16 -0300

    feat: setup cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,13 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
 
+app.register(cors, {
+  origin: true, // true allow all origins, right is the address of all envs
+})
 app.register(memoriesRoutes)
 
 app
